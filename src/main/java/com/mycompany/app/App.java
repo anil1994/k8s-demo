e7248ffb80449f79e1ca3f3cbaf8e46e3b020551
anil1994 <adalkilic@ku.edu.tr>
2022-04-11 00:11:01 +0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,7 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
-        wait(10000000000);
+        wait(10000);
     }
 
     private final String getMessage() {
